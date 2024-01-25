4a272544801d346823e23c5b2fdf81cb995ef8ce
enzonotario <enzo.notario@gmail.com>
2024-01-25 20:21:18 -0300

    change(echarts) use @pureadmin/utils

---
+++ docs/.vitepress/plugins/echarts.ts
@@ -0,0 +1,8 @@
+import { type App } from 'vue'
+import * as echarts from 'echarts'
+
+export function useECharts(app: App) {
+  app.config.globalProperties.$echarts = echarts
+}
+
+export default echarts
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,8 @@ import { theme, useOpenapi } from 'vitepress-theme-openapi'
 import DefaultTheme from 'vitepress/theme'
 import spec from '../../public/openapi.json' assert {type: 'json'}
 
+import { useECharts } from '../plugins/echarts'
+
 import ChartDolares from './components/ChartDolares.vue'
 import ChartFeriados from './components/ChartFeriados.vue'
 
@@ -14,6 +16,8 @@ export default {
     const openapi = useOpenapi()
     openapi.setSpec(spec)
     theme.enhanceApp({ app })
+
+    app.use(useECharts)
 
     app.component('ChartDolares', ChartDolares)
     app.component('ChartFeriados', ChartFeriados)
