87d2c223160bc12f613a1dcc650652f42f37d88d
enzonotario <enzo.notario@gmail.com>
2024-02-17 14:33:11 -0300

    feat(docs) github stars

---
+++ docs/.vitepress/theme/components/GitHubStars.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const githubStars = ref(null)
+
+onMounted(async () => {
+  const response = await fetch(
+    'https://api.github.com/repos/enzonotario/esjs-argentina-datos-api',
+  )
+  const data = await response.json()
+  githubStars.value = data.stargazers_count
+})
+</script>
+
+<template>
+  <div class="flex items-center px-2">
+    <a
+      href="https://github.com/enzonotario/esjs-argentina-datos-api"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="inline-flex items-center px-2.5 py-0.5 space-x-1 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 text-sm font-medium rounded border border-gray-300 dark:border-gray-700 hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    >
+      <span v-if="false" class="i-mdi-home w-5 h-5" />
+      <span class="i-mdi-star-outline w-5 h-5" />
+
+      <span>Stars: <span>{{ githubStars }}</span></span>
+    </a>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,12 +5,19 @@ import { es } from 'date-fns/locale'
 import spec from '../../public/openapi.json' assert { type: 'json' }
 import { useECharts } from '../plugins/echarts'
 import chartComponents from './components/charts'
+import { h } from 'vue'
+import GitHubStars from './components/GitHubStars.vue'
 
 import 'vitepress-theme-openapi/dist/style.css'
 import './style.css'
 
 export default {
   ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-after': () => h(GitHubStars),
+    })
+  },
   enhanceApp({ app }) {
     setDefaultOptions({ locale: es })
 
