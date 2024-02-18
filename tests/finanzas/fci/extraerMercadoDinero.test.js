bf6e09948548c7967d555df457d1ab96ff613e7e
enzonotario <enzo.notario@gmail.com>
2024-02-18 19:09:49 -0300

    chore(tests) extraerRendimientos

--- tests/finanzas/fci/extraerMercadoDinero.test.js
+++ tests/finanzas/fci/extraerMercadoDinero.test.js
@@ -3,12 +3,8 @@ import { extraerMercadoDinero } from '@/finanzas/fci/extraerMercadoDinero.esjs'
 
 describe('extrearFci', () => {
   it('extrae los fci', async () => {
-    try {
-      const items = await extraerMercadoDinero('2024-01-02')
+    const items = await extraerMercadoDinero('2024-01-02')
 
-      expect(items).toMatchSnapshot()
-    } catch (error) {
-      console.error(error)
-    }
+    expect(items).toMatchSnapshot()
   })
 })
---
+++ tests/finanzas/rendimientos/extraerRendimientos.test.js
@@ -0,0 +1,35 @@
+
+import { describe, expect, it } from 'vitest'
+import { extraerBuenbit } from '@/finanzas/rendimientos/extraerBuenbit.esjs'
+import { extraerFiwind } from '@/finanzas/rendimientos/extraerFiwind.esjs'
+import { extraerLetsbit } from '@/finanzas/rendimientos/extraerLetsbit.esjs'
+
+function testItems(items) {
+  expect(items.length).toBeGreaterThan(0)
+
+  for (const item of items) {
+    expect(item.moneda).toBeTypeOf('string')
+    expect(item.apy).toBeTypeOf('number')
+    expect(item.fecha).toBeTypeOf('string')
+  }
+}
+
+describe('extraerRendimientos', () => {
+  it('guarda buenbit', async () => {
+    const items = await extraerBuenbit()
+
+    testItems(items)
+  })
+
+  it('guarda fiwind', async () => {
+    const items = await extraerFiwind()
+
+    testItems(items)
+  })
+
+  it('guarda letsbit', async () => {
+    const items = await extraerLetsbit()
+
+    testItems(items)
+  })
+})
