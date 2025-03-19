e0feba1dbc02171b990d99898dfcf220fdd88587
enzonotario <enzo.notario@gmail.com>
2025-03-19 19:27:04 -0300

    change(diputados): minified json

--- packages/core/src/utils/writeEndpoint.ts
+++ packages/core/src/utils/writeEndpoint.ts
@@ -4,7 +4,7 @@ import { DATOS_PATH } from '../constants.ts'
 
 export function writeEndpoint(endpoint: string, data: any): string {
   const filePath = path.join(DATOS_PATH, ...endpoint.split('/'), 'index.json')
-  const jsonData = JSON.stringify(data, null, 4)
+  const jsonData = JSON.stringify(data)
 
   fs.mkdirSync(path.dirname(filePath), { recursive: true })
   fs.writeFileSync(filePath, jsonData, 'utf-8')
